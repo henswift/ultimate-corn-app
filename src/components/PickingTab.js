df5df4e5d27e32a7ccbe9f2c8941dd12dc67985e
Hen Swift <145405400+henswift@users.noreply.github.com>
2023-12-11 18:21:44 -0500

    Pushing everything before I open another app.

--- src/components/PickingTab.js
+++ src/components/PickingTab.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Calculator from './Calculator'
 
 function Picking() {
   return (
     <div className='Home-tab'>
       picking
+      <Calculator />
       </div>
   )
 }
