d72833aa1f3aba1631aadfba5f2c4abbfafb1861
Hen Swift <145405400+henswift@users.noreply.github.com>
2023-12-14 18:20:28 -0500

    Added alt text

--- src/pages/homepage/HomeTab.js
+++ src/pages/homepage/HomeTab.js
@@ -5,7 +5,7 @@ function HomeTab() {
   return (
     <div className='Home-tab'>
         <div className='Default-box'>
-          <img src='Sweet Corn Sticker.png'></img>
+          <img src='Sweet Corn Sticker.png' alt="A smiling corn face"></img>
           <h3>Welcome to CornTop!</h3>
         </div>
     </div>
