57f27a1413a024e6c93055d711289c82a26ea4a4
Hen Swift <145405400+henswift@users.noreply.github.com>
2023-12-14 18:54:29 -0500

    Added some content to the home page

--- src/pages/homepage/HomeTab.js
+++ src/pages/homepage/HomeTab.js
@@ -5,7 +5,6 @@ function HomeTab() {
   return (
     <div className='Home-tab'>
         <div className='Default-box'>
-          <img src='Sweet Corn Sticker.png' alt="A smiling corn face"></img>
           <h3>Welcome to CornTop!</h3>
         </div>
     </div>
