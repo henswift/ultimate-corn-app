75c344b8eaff924b2a082dbb649e415e677c1820
Hen Swift <145405400+henswift@users.noreply.github.com>
2023-12-22 19:25:54 -0500

    closing down for the night

--- src/pagecomponents/PlantingDivTotal.js
+++ src/pagecomponents/PlantingDivTotal.js
@@ -10,28 +10,20 @@ function PlantingDivTotal() {
   const [selectedPlanting, setSelectedPlanting] = useState(null);
 
   return (
-    <div className='Planting-tab'>
-      <div className='Item-section'>
-        <div className='Section-titles'>
-          <h3>Plantings</h3>
-          <h3>+ Plantings</h3>
-        </div>
-        <div className='Section-contents'>
-          {plantings.map((planting) => (
-            <Link
-              to={`/planting/${planting.id}`}
-              key={planting.id}
-              onClick={() => setSelectedPlanting(planting)}
-            >
-              <div className='Section-default-box'>
-                <p>{planting.name}</p>
-                <img src={planting.image} alt='corn' />
-              </div>
-            </Link>
-          ))}
-          {selectedPlanting && <ViewPlanting planting={selectedPlanting} />}
-        </div>
-      </div>
+    <div className='Section-container'>
+      {plantings.map((planting) => (
+        <Link
+          to={`/planting/${planting.id}`}
+          key={planting.id}
+          onClick={() => setSelectedPlanting(planting)}
+        >
+          <div className='Section-default-box'>
+            <p>{planting.name}</p>
+            <img src={planting.image} alt='corn' />
+          </div>
+        </Link>
+      ))}
+      {selectedPlanting && <ViewPlanting planting={selectedPlanting} />}
     </div>
   );
 }
